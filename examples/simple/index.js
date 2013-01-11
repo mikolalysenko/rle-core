59c7a2a876578b3eff93786f186a06ce7e03544f
Mikola Lysenko <mikolalysenko@gmail.com>
2013-01-11 10:51:46 -0600

    surface is working, also pulled out test code framework into separate repository

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -1,14 +1,15 @@
 var $         = require("jquery-browserify")
-  , rle       = require("../../src/index.js")
-  , examples  = require("../common/examples.js");
+  , rle       = require("../../src/index.js");
 
 $(document).ready(function() {
 
   //Create viewer
-  var viewer = require("../common/viewer.js").makeViewer();
+  var viewer = require("gl-shells").makeViewer();
   
   //Create a volume
-  var volume = rle.sample([-8,-8,-8], [9,9,9], examples.sphere);
+  var volume = rle.sample([-6,-6,-6], [7,7,7], function(x) {
+    return 5 - Math.sqrt(x[0]*x[0]+x[1]*x[1]+x[2]*x[2]);
+  });
   viewer.updateMesh(rle.surface(volume));
   
 });
